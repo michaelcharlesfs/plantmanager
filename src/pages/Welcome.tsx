fa766ce163ac253f52b1d206173a932156415515
Michael Charles <michael.silva.ext@sascar.com.br>
2021-04-20 09:29:29 -0300

    Adicionar fontes e tela de carregamento

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,37 +1,42 @@
 import React from 'react';
-import {SafeAreaView, Text, Image, TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
+import {SafeAreaView, View, Text, Image, TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
 import {Feather} from '@expo/vector-icons';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil
-            </Text>
-            
-            <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de {'\n'}
+                    forma fácil
+                </Text>
+                
+                <Image source={wateringImg} style={styles.image} resizeMode="contain" />
 
-            <Text style={styles.subtitle}>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.</Text>
+                <Text style={styles.subtitle}>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.</Text>
 
-            <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-            <Text>
-                <Feather name="chevron-right" style={styles.buttonIcon} />
-            </Text>
-        </TouchableOpacity>
+                <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+                    <Feather name="chevron-right" style={styles.buttonIcon} />
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    wrapper: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+        paddingHorizontal: 20
     },
     title: {
         fontSize: 28,
@@ -39,13 +44,16 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: colors.heading,
         marginTop: 38,
-        width: '100%'
+        width: '100%',
+        fontFamily: fonts.heading,
+        lineHeight: 34
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 16,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
     },
     image: {
         width: Dimensions.get('window').width * 0.7,
@@ -64,4 +72,4 @@ const styles = StyleSheet.create({
         color: colors.white,
         fontSize: 24
     }
-})
+});
