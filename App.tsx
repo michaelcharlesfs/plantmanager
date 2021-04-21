576dbb25e310ff3563d79975e1bf4f8e3fbe2630
Michael Charles <michael.silva.ext@sascar.com.br>
2021-04-21 14:13:47 -0300

    Adicionar paginas de confirmacao e identificação integradas com rotas

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
-import { Welcome } from './src/pages/Welcome';
 import { useFonts, Jost_400Regular, Jost_600SemiBold} from '@expo-google-fonts/jost';
+import Routes from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,6 +16,6 @@ export default function App() {
   }
 
   return (
-    <Welcome />
+    <Routes />
   );
 }
